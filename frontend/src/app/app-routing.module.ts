86f2aeb04c46a7c0760c61a8b9c10b6a4e89791f
Rinkal-2024 <159881222+Rinkal-2024@users.noreply.github.com>
2024-05-09 15:49:51 +0530

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'map',
     pathMatch: 'full'
+  },
+  { 
+    path: '', 
+    component:AppComponent
   },
   {
     path: 'folder/:id',
